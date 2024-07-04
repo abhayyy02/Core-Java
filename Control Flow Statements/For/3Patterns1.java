4ae9014d1ddeb34cae092a387d9a396fd1e377c1
Abhay Jadhav <abhayjadhav1221@gmail.com>
2024-07-04 08:21:14 +0530

    Add files via upload

---
+++ Control Flow Statements/For/3Patterns1.java
@@ -0,0 +1,72 @@
+/*
+
+* * * *
+* * * *																			1st Pattern.
+* * * *
+* * * *
+
+class Demo3
+{
+	public static void main(String[] args) 
+ 	{
+		for(int i=1;i<=4;i++)
+	{
+			for(int j=1; j<=4; j++)
+ 			{
+ 				System.out.print("* ");
+ 			}
+
+ 			System.out.println();
+ 		}	
+  	}
+
+ }
+
+
+
+*																				2nd Pattern.
+* *
+* * *
+* * * * 
+
+
+class Demo3
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<=4; i++ )
+		{
+			for(int j=1; j<=i ;j++)
+			{
+				System.out.print("* ");
+			}
+
+			System.out.println();
+		}	
+	}
+}
+
+
+1
+1 1
+1 1 1
+1 1 1 1 																		3rd Pattern. 
+
+class Demo3
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<=4; i++)
+		{
+			int num1=1;
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(num1+" ");
+			}
+			System.out.println();
+		}	
+	}
+}
+
+*/
+
