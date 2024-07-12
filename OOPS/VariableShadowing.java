82c2862623655f1c5e8a4cdabf15662812ea8ada
Abhay Jadhav <abhayjadhav1221@gmail.com>
2024-07-12 20:56:36 +0530

    Add files via upload

---
+++ OOPS/VariableShadowing.java
@@ -0,0 +1,23 @@
+class Parent
+{
+	int a =10;										
+}
+
+class Child extends Parent
+{
+	int a =20;
+	void m1()
+	{
+		int a =30;
+		System.out.println(a);
+		System.out.println(this.a);
+		System.out.println(super.a);
+	}
+
+	public static void main(String[] args) 
+	{
+		Child obj = new Child();
+		obj.m1();	
+	}
+}
+
